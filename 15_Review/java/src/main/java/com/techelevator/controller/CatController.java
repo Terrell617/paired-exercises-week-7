5090dd8d0cd5e3ba9c857836261d6485539750f9
Makkah Beasley <makkahbeasley@gmail.com>
2021-06-25 11:34:14 -0400

    part 1

--- 15_Review/java/src/main/java/com/techelevator/controller/CatController.java
+++ 15_Review/java/src/main/java/com/techelevator/controller/CatController.java
@@ -1,9 +1,17 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.CatCardDao;
+import com.techelevator.model.CatCard;
+import com.techelevator.model.CatFact;
+import com.techelevator.model.CatPic;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CatController {
 
     private CatCardDao catCardDao;
@@ -16,5 +24,14 @@ public class CatController {
         this.catPicService = catPicService;
     }
 
-
+    @RequestMapping(path = "/api/cards/random", method = RequestMethod.GET)
+    public CatCard randomCard() {
+        CatFact fact = catFactService.getFact();
+        CatPic picture = catPicService.getPic();
+        CatCard card = new CatCard();
+        card.setCatFact("This is a cat");
+        card.setImgUrl(picture.getFile());
+        return card;
+    }
 }
+
